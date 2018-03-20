1101d4a103113f80973b48eba46f8274bcecfd36
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-03-20 23:23:19 +0200

    finished stage2 form

--- script/index_controls.js
+++ script/index_controls.js
@@ -8,13 +8,17 @@ function SubmitForm(){
 
         if (style.display == "block"){  
             forms[i].style.display = "none";
+            if (i != forms.length - 1){
+                forms[i + 1].style.display = "block";
+            }
+            break;
         }
     }
 }
 
 function ToggleIssueForm(){
     var button = document.getElementById("btn-show-add-issue-form");
-    var issueForm = document.getElementsByClassName("add-issue")[0];
+    var issueForm = document.getElementsByClassName("add-issue-bottom")[0];
 
     if (issueForm.classList.contains("issue-bottom-hidden")){
         issueForm.classList.add("issue-bottom-shown");
@@ -26,4 +30,12 @@ function ToggleIssueForm(){
         issueForm.classList.add("issue-bottom-hidden");
         button.innerHTML = "Raporteaza o problema";
     }
+}
+
+function AddFormImage(){
+    var newImage = document.createElement("img");
+    var addImageButton = document.getElementById("stage2-form-add-image");
+    newImage.classList.add("stage2-form-image");
+
+    addImageButton.insertAdjacentElement("beforebegin", newImage);
 }
