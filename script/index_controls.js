d4c08a7103aff226ca6c8f249bd9e1d3fcbced99
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-03-20 19:55:48 +0200

    change add issue toggle text on click

--- script/index_controls.js
+++ script/index_controls.js
@@ -13,14 +13,17 @@ function SubmitForm(){
 }
 
 function ToggleIssueForm(){
+    var button = document.getElementById("btn-show-add-issue-form");
     var issueForm = document.getElementsByClassName("add-issue")[0];
 
     if (issueForm.classList.contains("issue-bottom-hidden")){
         issueForm.classList.add("issue-bottom-shown");
         issueForm.classList.remove("issue-bottom-hidden");
+        button.innerHTML = "Anuleaza";
     }
     else{
         issueForm.classList.remove("issue-bottom-shown");
         issueForm.classList.add("issue-bottom-hidden");
+        button.innerHTML = "Raporteaza o problema";
     }
 }
