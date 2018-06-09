c08480b36160b1af538186ed45c7213ffdf4a067
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-06-09 15:18:04 +0300

    app work

--- api/app.js
+++ api/app.js
@@ -1,55 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const http = require('http');
+const server = http.createServer().listen(3000);
 
-http.createServer(function (request, response) {
-    console.log(require('path').dirname(require.main.filename));
-    console.log('request ', request.url);
+server.on('request', (req, res) => {
+    filterRequest(req, res);
+});
 
-    var filePath = '.' + request.url;
-    if (filePath == './') {
-        filePath = './../front/index.html';
+function filterRequest(req, res) {
+    if (req.method == "GET") {
+        processGetRequest(req, res);
     }
+    if (req.method == "POST") {
+        processPostRequest(req, res);
+    }
+};
 
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.svg': 'application/image/svg+xml'
-    };
+function processGetRequest(req, res) {
+    res.write('primit get');
+    req.pipe(res);
+}
 
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+function processPostRequest(req, res) {
+    res.write('Post la ' + req.url);
+}
--- front/script/map.js
+++ front/script/map.js
@@ -1,27 +1,29 @@
 var map;
-let popups = [];
 
 window.onload = init();
 
 function init() {
-  var script = document.createElement("script");
-  script.src =
-    "https://maps.googleapis.com/maps/api/js?key=AIzaSyDamjhWg7wcyotS8tgBoI69RFOx9onVpDs&callback=initMap";
-  document.head.appendChild(script);
+	var script = document.createElement("script");
+	script.src =
+		"https://maps.googleapis.com/maps/api/js?key=AIzaSyDamjhWg7wcyotS8tgBoI69RFOx9onVpDs&callback=initMap";
+	document.head.appendChild(script);
 
-  addPopupsToMap();
+	addPopupsToMap();
 }
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.866, lng: 151.196 },
-    zoom: 8
-  });
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: {
+			lat: -33.866,
+			lng: 151.196
+		},
+		zoom: 8
+	});
 }
 
-function addPopupsToMap(){
-    popup = new Popup(
-        new google.maps.LatLng(-33.866, 151.196),
-        document.getElementById('popups'));
-    popup.setMap(map);
+function addPopupsToMap() {
+	popup = new Popup(
+		new google.maps.LatLng(-33.866, 151.196),
+		document.getElementById('popups'));
+	popup.setMap(map);
 }
