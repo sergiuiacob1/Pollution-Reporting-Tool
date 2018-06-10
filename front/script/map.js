76f2bb1f2749c9d3366b07b6eb753c47694cb127
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-06-10 12:37:28 +0300

    center map to user location

--- front/script/map.js
+++ front/script/map.js
@@ -8,8 +8,6 @@ function init() {
 	script.src =
 		"https://maps.googleapis.com/maps/api/js?key=AIzaSyDamjhWg7wcyotS8tgBoI69RFOx9onVpDs&callback=initMap";
 	document.head.appendChild(script);
-
-	getReports();
 }
 
 function initMap() {
@@ -20,26 +18,68 @@ function initMap() {
 		},
 		zoom: 8
 	});
+
+	centerMapToUserLocation();
+
+	getReports();
+}
+
+function centerMapToUserLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+
+			map.panTo(pos);
+		});
+	}
+}
+
+
+function placeMarker(position, map) {
+	var marker = new google.maps.Marker({
+		position: position,
+		map: map
+	});
+	map.panTo(position);
 }
 
 function getReports() {
 	var getURL = hostname + "/api/reports";
-
-	console.log ('getting');
 
 	$.ajax({
 		url: getURL,
 		type: 'GET',
 		dataType: 'json'
 	}).done(function (result) {
-		console.log('a mers');
+		console.log('am luat reports');
 		addReportsToMap(result);
 	}).fail(function () {
-		console.log('ceva nu a mers bine');
+		console.log('get reports a esuat');
 	});
 }
 
 function addReportsToMap(reports) {
-	console.log(reports);
-	console.log('sadasda');
+
+	var uluru = {
+		lat: -25.363,
+		lng: 131.044
+	};
+
+	var infowindow = new google.maps.InfoWindow({
+		content: 'SALUT'
+	});
+
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map,
+		title: 'Uluru (Ayers Rock)'
+	});
+	marker.addListener('click', function () {
+		infowindow.open(map, marker);
+	});
+
+	map.panTo(uluru);
 }
