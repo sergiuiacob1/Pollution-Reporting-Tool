038a2c8c0adf7217c1b5aecee55fd525d3889c98
Sergiu Iacob <sergiu.iacob1@gmail.com>
2018-06-11 18:22:15 +0300

    more bug fixes

--- front/script/register.js
+++ front/script/register.js
@@ -5,28 +5,31 @@ $(document).ready(function () {
     btnLogIn.onclick = register;
 });
 
-function register () {
+function register() {
     let email = document.getElementById("input_form_email");
     let name = document.getElementById("input_form_name");
     let surname = document.getElementById("input_form_surname");
     let password = document.getElementById("input_form_password");
 
-    let object = {email:email.value,name:name.value,surname:surname.value,password:password.value};
+    let object = {
+        email: email.value,
+        name: name.value,
+        surname: surname.value,
+        password: password.value
+    };
     console.log('Sending request from front : ');
     console.log(object);
 
 
     $.post(hostname + '/register', JSON.stringify(object))
-        .done(function (res,status) {
+        .done(function (res, status) {
             console.log(res);
 
-            if(res.success)
-            {
-                localStorage.setItem("token",res.token);
-                localStorage.setItem("self",JSON.stringify(res.self));
+            if (res.success === true) {
+                localStorage.setItem("token", res.token);
+                localStorage.setItem("self", JSON.stringify(res.self));
                 window.location.replace("http://91.92.128.27/front/pages/map.html");
-            }
-            else {
+            } else {
                 alert("Error signing up!");
             }
         });
